842320cbad5dc4986912eb689a0558ddf18b6bbc
eirsture <eirik.sture@gmail.com>
2020-09-09 13:55:19 +0200

    remove unused import

--- app/clubs/views.py
+++ app/clubs/views.py
@@ -1,12 +1,12 @@
-from django.views.decorators.csrf import csrf_exempt
-
 from .models import Club
 from .serializers import ClubSerializer
 from rest_framework import viewsets
 
 # Create your views here.
 
-#Automatically provides 'list', 'create', 'retrieve', 'update' and 'destroy' actions
+# Automatically provides 'list', 'create', 'retrieve', 'update' and 'destroy' actions
+
+
 class ClubViewSet(viewsets.ModelViewSet):
     queryset = Club.objects.all()
     serializer_class = ClubSerializer
