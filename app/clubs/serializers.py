4e7425cfa088c0f9b7cbe7c4f4f0c68405ff6c57
Jenny Yu <jennyjinyiyu@gmail.com>
2020-09-09 14:44:30 +0200

    removed url form fields in ClubSerializer

--- app/clubs/serializers.py
+++ app/clubs/serializers.py
@@ -6,7 +6,7 @@ class ClubSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Club
-        fields = ['url', 'id', 'name', 'contact_email', 'contact_phone', 'pricing', 'register_info']
+        fields = ['id', 'name', 'contact_email', 'contact_phone', 'pricing', 'register_info']
         """
             The source argument controls which attribute is used to populate a field, 
             and can point at any attribute on the serialized instance.
