89c71090a0fa92936737f4093d22ba6d5b8f6965
Odd Andre Owren <oddandreowren@gmail.com>
2020-09-11 11:01:24 +0200

    init docker

--- app/app/settings.py
+++ app/app/settings.py
@@ -90,8 +90,12 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
