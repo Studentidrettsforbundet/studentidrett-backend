e3a1ea0d3297c3e3e084341f5dfe34bbef214de4
eirsture <37185897+eirsture@users.noreply.github.com>
2020-09-18 14:50:25 +0200

    Feat/filter clubs by city (#17)
    * add tests for city query param
    
    * added functionality to search for clubs by cities
    
    * add tests for finding cities based on region
    
    * added choices for region
    
    * find cities by region query_param
    
    * added migrations
    
    * refactor and cleaning code
    
    * changed Ø to O

--- app/cities/models.py
+++ app/cities/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-
+from app.enums import Region
 # Create your models here.
 
 
 class City(models.Model):
     name = models.CharField(max_length=31, null=False)
-    region = models.CharField(max_length=63, null=False)
+    region = models.CharField(max_length=4, null=False, choices=Region.choices)
 
     def __str__(self):
         return self.name
--- app/clubs/tests.py
+++ app/clubs/tests.py
@@ -3,6 +3,7 @@ from rest_framework.test import APIRequestFactory, APITestCase
 
 from clubs.models import Club
 from clubs.views import ClubViewSet
+from cities.models import City
 
 
 # Create your tests here.
@@ -11,8 +12,12 @@ from clubs.views import ClubViewSet
 class TestClubsApi(APITestCase):
 
     def setUp(self):
-        club = Club(name="NTNUI")
+        city = City(id=5, name="Trondheim")
+        city.save()
+        club = Club(name="NTNUI", city=city)
         club.save()
+        club2 = Club(name="Omega Fotballklubb", city=city)
+        club2.save()
         self.factory = APIRequestFactory()
 
     def test_clubs_list(self):
@@ -23,10 +28,12 @@ class TestClubsApi(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check pagination
         self.assertEqual(response.data.keys(), {'count', 'next', 'previous', 'results'})
-        # Check fields in result
+        # Check fields in results
         self.assertEqual(response.data.get('results')[0].keys(), {'id', 'city', 'name', 'information',
                                                                   'contact_email', 'contact_phone',
                                                                   'pricing', 'register_info'})
+        # Check length of results
+        self.assertEqual(len(response.data.get('results')), 2)
 
     def test_club_detail(self):
         request = self.factory.get('clubs')
@@ -45,3 +52,37 @@ class TestClubsApi(APITestCase):
         response = view(request, pk='999')
         # Check status code
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_query_param_city(self):
+        request = self.factory.get('clubs', {'city': 'Trondheim'})
+        view = ClubViewSet.as_view({'get': 'list'})
+        response = view(request)
+        # Check status code
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check length of results
+        self.assertEqual(len(response.data.get('results')), 2)
+        # Check that both clubs are in Trondheim
+        self.assertEqual(response.data.get('results')[0].get('city'), 5)
+        self.assertEqual(response.data.get('results')[1].get('city'), 5)
+
+
+
+    def test_query_param_city_no_clubs(self):
+        new_city = City(name="Oslo")
+        new_city.save()
+        request = self.factory.get('clubs', {'city': 'Oslo'})
+        view = ClubViewSet.as_view({'get': 'list'})
+        response = view(request)
+        # Check status code
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check length of results
+        self.assertEqual(len(response.data.get('results')), 0)
+
+    def test_query_param_non_existing_city(self):
+        request = self.factory.get('clubs', {'city': 'Gotham'})
+        view = ClubViewSet.as_view({'get': 'list'})
+        response = view(request)
+        # Check status code
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check length of results
+        self.assertEqual(len(response.data.get('results')), 0)
