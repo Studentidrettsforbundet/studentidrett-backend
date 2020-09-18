ecb955751f7cf78f9b42d97d4dcc24f183ea809f
Jenny Yu <jennyjinyiyu@gmail.com>
2020-09-18 14:32:43 +0200

    changed sport to many_to_many type

--- app/groups/models.py
+++ app/groups/models.py
@@ -10,11 +10,10 @@ class Group(models.Model):
     name = models.CharField(max_length=30, blank=False, default='Group')
     description = models.TextField(max_length=500, null=True)
     cover_photo = models.ImageField(upload_to='groups', null=True)
-    sport_type = models.ForeignKey(Sport, on_delete=models.SET_NULL, null=True)
+    sport_type = models.ManyToManyField(Sport, on_delete=models.SET_NULL, null=True, through="sport_in_group")
     club = models.ForeignKey(Club, on_delete=models.SET_NULL, null=True, related_name="groups") #TODO on_delete=models.CASCADE,
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, related_name="groups")#TODO on_delete=models.CASCADE,
-    #contact_person = models.CharField(max_length=30, blank=True, default='', null=True)
-    #contact_email = models.EmailField(max_length=254, null=True)
+
 
     class Meta:
         ordering = ['name']
