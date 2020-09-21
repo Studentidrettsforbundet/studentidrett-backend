a61906c9285b0c2dd81c1525deae0a3ca2b33b3a
Odd Andre Owren <oddandreowren@gmail.com>
2020-09-21 21:24:02 +0200

    fix docker

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -181,3 +181,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
