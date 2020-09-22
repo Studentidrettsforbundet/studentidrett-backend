22330b0f5133fc4ec8fdb6aef1bb11e4d2946a2f
Jenny Yu <37080561+jennyjyu@users.noreply.github.com>
2020-09-22 14:12:47 +0200

    Feat/groups (#24)
    * added city (location) as a foreign key to the group model
    
    * changing testing from client to factory
    
    * working tests with apifactory
    
    * changed sport to many_to_many type
    
    * renamed the manytomanyfield from sport_type to sports. Tests not working yet
    
    * working tests for manytomany field sports
    
    * added tests for m2m field
    
    * removed unecessary comment
    
    * installed corsheaders and froze requirements
    
    * change from relative to absolute imports, and delete unused imports

--- app/groups/models.py
+++ app/groups/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from sports.models import Sport
 from clubs.models import Club
+from cities.models import City
 
 # Create your models here.
 
 
 class Group(models.Model):
-    name = models.CharField(max_length=30, blank=False, default='Sportsklubb')
+    name = models.CharField(max_length=30, blank=False, default='Group')
     description = models.TextField(max_length=500, null=True)
     cover_photo = models.ImageField(upload_to='groups', null=True)
-    sport_type = models.ForeignKey(Sport, on_delete=models.SET_NULL, null=True)
-    club = models.ForeignKey(Club, on_delete=models.CASCADE)
-    contact_person = models.CharField(max_length=30, blank=True, default='', null=True)
-    contact_email = models.EmailField(max_length=254, null=True)
+    sports = models.ManyToManyField(Sport)
+    club = models.ForeignKey(Club, on_delete=models.SET_NULL, null=True, related_name="groups") #TODO on_delete=models.CASCADE,
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, related_name="groups")#TODO on_delete=models.CASCADE,
+
 
     class Meta:
         ordering = ['name']
