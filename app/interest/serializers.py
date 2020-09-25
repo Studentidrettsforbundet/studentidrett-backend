10265b8b5577b55f881bd571fa520981d8a940e3
eirsture <37185897+eirsture@users.noreply.github.com>
2020-09-25 10:18:48 +0200

    Feat/interest (#27)
    * update to new interest model
    
    * Use cookies to save Interest objects
    
    * Clean-up code
    
    Co-authored-by: Odd Andre Owren <oddandreowren@gmail.com>

--- app/interest/serializers.py
+++ app/interest/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from .models import Interest
 
 
+class CurrentCookieDefault(object):
+    """ Because cookie_key is read_only in addition to being in a UniqueTogether-relationship,
+        it introduces a special case that can be fixed as suggested by:
+        https://stackoverflow.com/a/56061820
+    """
+
+    def set_context(self, serializer_field):
+        self.cookie_key = serializer_field.context['request'].COOKIES.get('csrftoken')
+
+    def __call__(self):
+        return self.cookie_key
+
+
 class InterestSerializer(serializers.ModelSerializer):
+    cookie_key = serializers.CharField(read_only=True, default=CurrentCookieDefault())
+
     class Meta:
         model = Interest
-        fields = ['id', 'email', 'group']
+        fields = ['id', 'cookie_key', 'group', 'created']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Interest.objects.all(),
+                fields=['cookie_key', 'group']
+            )
+        ]
--- app/interest/tests.py
+++ app/interest/tests.py
@@ -2,13 +2,10 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate
 
-from .models import Interest
-from .views import InterestViewSet
+from interest.models import Interest
+from interest.views import InterestViewSet
 from clubs.models import Club
 from groups.models import Group
-
-
-# Create your tests here.
 
 
 class TestInterestApi(APITestCase):
@@ -20,7 +17,7 @@ class TestInterestApi(APITestCase):
         self.group.save()
         user = User.objects.create_superuser('testuser', email='testuser@test.com', password='testing')
         user.save()
-        self.interest = Interest(email='test@test.no', group=self.group)
+        self.interest = Interest(cookie_key='c00k13M0n5t3r', group=self.group)
         self.interest.save()
 
         self.factory = APIRequestFactory()
@@ -31,25 +28,27 @@ class TestInterestApi(APITestCase):
         self.get_detail_view = InterestViewSet.as_view({'get': 'retrieve'})
 
     def test_post_interests(self):
-        request = self.factory.post('/interest/', {'email': 'test@test.no', 'group': self.group.pk}, format='json')
+        request = self.factory.post('/interest/', {'group': self.group.pk}, format='json')
+        request.COOKIES['csrftoken'] = 'c00k13M0n5t3r1sN0M0r3'
         response = self.post_view(request)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data.keys(), {'id', 'email', 'group'})
+        self.assertEqual(response.data.keys(), {'id', 'cookie_key', 'group', 'created'})
 
-    def test_post_wrong_email(self):
-        request = self.factory.post('/interest/', {'email': 'test', 'group': self.group.pk}, format='json')
+    def test_post_used_cookie_key(self):
+        request = self.factory.post('/interest/', {'group': self.group.pk}, format='json')
+        request.COOKIES['csrftoken'] = 'c00k13M0n5t3r'
         response = self.post_view(request)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data.keys(), {'email'})
+        self.assertEqual(response.data.keys(), {'non_field_errors'})
 
-    def test_post_empty_email(self):
-        request = self.factory.post('/interest/', {'email': '', 'group': self.group.pk}, format='json')
+    def test_post_empty_cookie_key(self):
+        request = self.factory.post('/interest/', {'group': self.group.pk}, format='json')
         response = self.post_view(request)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data.keys(), {'email'})
 
     def test_post_empty_club(self):
-        request = self.factory.post('/interest/', {'email': 'test@test.no', 'group': None}, format='json')
+        request = self.factory.post('/interest/', {'group': None}, format='json')
+        request.COOKIES['csrftoken'] = 'testestest'
         response = self.post_view(request)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data.keys(), {'group'})
@@ -79,7 +78,7 @@ class TestInterestApi(APITestCase):
         force_authenticate(request, self.user)
         response = self.get_detail_view(request, pk=self.interest.pk)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data.keys(), {'id', 'email', 'group'})
+        self.assertEqual(response.data.keys(), {'id', 'cookie_key', 'group', 'created'})
 
     def test_get_non_existing_interest(self):
         request = self.factory.get('/interest/')
