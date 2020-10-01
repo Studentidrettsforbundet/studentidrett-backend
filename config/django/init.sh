81f740ea7791822b2a27c9e43b1e3a1a95e73dd4
Odd Andre Owren <oddandreowren@gmail.com>
2020-10-02 01:02:16 +0200

    add db health-check to init.sh, update docker-c

--- config/django/init.sh
+++ config/django/init.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 set -e
 
+# wait for Postgres to start
+# wait for Postgres to start
+function postgres_ready() {
+python << END
+import sys
+import os
+import psycopg2
+try:
+    conn = psycopg2.connect(dbname=os.environ.get('DBNAME'),
+    user=os.environ.get('DBUSER'), password=os.environ.get('DBPASS'), host=os.environ.get('DBHOST'))
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+until postgres_ready; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+# Start app
+>&2 echo "Postgres is up - executing command"
+
 echo "Starting SSH ..."
 service ssh start
 
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
 
-gunicorn --bind 0.0.0.0:80 app.wsgi
+python manage.py collectstatic --noinput
+
+gunicorn wsgi -w 4 -b 0.0.0.0:8000 --chdir=/code/app --log-file -
