929223163fda6a009c7d05d4c82fc459d5c5a24b
eirsture <eirik.sture@gmail.com>
2020-10-01 14:37:37 +0200

    Clean up attributes in model fields

--- app/groups/models.py
+++ app/groups/models.py
@@ -8,17 +8,17 @@ from sports.models import Sport
 
 
 class Group(models.Model):
-    name = models.CharField(max_length=30, blank=False, default="Group")
+    name = models.CharField(max_length=40, blank=False, null=False, default="Group")
     description = models.TextField(max_length=500, null=True)
     cover_photo = models.ImageField(upload_to="groups", null=True)
     sports = models.ManyToManyField(Sport)
     club = models.ForeignKey(
-        Club, on_delete=models.SET_NULL, null=True, related_name="groups"
-    )  # TODO on_delete=models.CASCADE,
+        Club, on_delete=models.CASCADE, null=True, related_name="groups"
+    )
     city = models.ForeignKey(
-        City, on_delete=models.SET_NULL, null=True, related_name="groups"
-    )  # TODO on_delete=models.CASCADE,
-    contact_email = models.EmailField(max_length=254, null=True)
+        City, on_delete=models.CASCADE, null=True, related_name="groups"
+    )
+    contact_email = models.EmailField(max_length=40, null=True)
 
     class Meta:
         ordering = ["name"]
--- app/sports/models.py
+++ app/sports/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Sport(models.Model):
-    name = models.CharField(max_length=100, blank=False, null=False)
+    name = models.CharField(max_length=40, blank=False, null=False)
 
     class Meta:
         ordering = ["name"]
