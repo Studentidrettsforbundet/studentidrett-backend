b0cef48e541fa971a69def486d6260ec2acfc123
Odd Andre Owren <oddandreowren@gmail.com>
2020-10-05 14:20:03 +0200

    update DB-variable in init.sh

--- config/django/init.sh
+++ config/django/init.sh
@@ -9,7 +9,7 @@ import sys
 import os
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname=os.environ.get('POSTGRES_NAME'),
+    conn = psycopg2.connect(dbname=os.environ.get('POSTGRES_DB'),
     user=os.environ.get('POSTGRES_USER'), password=os.environ.get('POSTGRES_PASSWORD'),
     host=os.environ.get('POSTGRES_HOST'))
 except psycopg2.OperationalError:
