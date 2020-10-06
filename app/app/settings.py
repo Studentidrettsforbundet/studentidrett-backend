3e2480be235d5f811170223c57323e6a78786c5a
Odd Andre Owren <oddandreowren@gmail.com>
2020-10-06 15:24:02 +0200

    remove BASE_DIR from static_root

--- app/app/settings.py
+++ app/app/settings.py
@@ -173,6 +173,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATIC_ROOT = "/staticfiles/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "./media"
