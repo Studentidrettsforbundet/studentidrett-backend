01a5a13e0a5a9a28a8638cc5eefa19cc17e53dfb
Odd Andre Owren <oddandreowren@gmail.com>
2020-10-06 15:16:27 +0200

    change static dirs to work in deployment

--- app/app/settings.py
+++ app/app/settings.py
@@ -172,10 +172,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/staticfiles/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "./media"
