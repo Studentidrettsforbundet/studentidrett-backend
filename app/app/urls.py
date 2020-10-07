8138b1b307cc27879b336ce96fbddd53ea47be8c
Odd Andre Owren <oddandreowren@gmail.com>
2020-10-07 11:46:01 +0200

    fix typo in urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -32,5 +32,5 @@ urlpatterns = (
         path("", include("teams.urls")),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + static(settings.MEDIA_URTL, document_root=settings.MEDIA_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 )
