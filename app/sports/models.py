8d9f3934914572093ab820078027983e3b56286e
eirsture <eirik.sture@gmail.com>
2020-10-09 11:35:30 +0200

    add __str__ for sport

--- app/sports/models.py
+++ app/sports/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Sport(models.Model):
     name = models.CharField(max_length=40, blank=False, null=False)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ["name"]
