3f46a10be07473bedeb6ff7212556268d961be85
Odd Andre Owren <oddandreowren@gmail.com>
2020-10-21 15:50:39 +0200

    adjust label-serializer to show list of sports and alternatives, add check to prevent duplicate sports

--- app/sports/models.py
+++ app/sports/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Sport(models.Model):
-    name = models.CharField(max_length=40, blank=False, null=False, unique=True)
+    name = models.CharField(max_length=40, blank=False, null=False)
 
     def __str__(self):
         return self.name
