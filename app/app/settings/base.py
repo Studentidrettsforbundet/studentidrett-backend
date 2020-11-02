5d6b9354508a1e79ee3813af1b07d02d842d5440
Odd Andre Owren <oddandreowren@gmail.com>
2020-11-02 11:24:25 +0100

    update nginx and docker to serve media-files

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -191,4 +191,3 @@ STATICFILES_DIR = os.path.join(BASE_DIR, "/staticfiles/")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "/mediafiles")
-MEDIAFILES_DIR = os.path.join(BASE_DIR, "/mediafiles")
