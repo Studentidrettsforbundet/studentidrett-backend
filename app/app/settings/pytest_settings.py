6293721f1af3ceb7dd2908104af669560015087a
eirsture <eirik.sture@gmail.com>
2020-11-18 20:58:52 +0100

    attempt to fix github actions test error

--- app/app/settings/pytest_settings.py
+++ app/app/settings/pytest_settings.py
@@ -2,17 +2,6 @@
 from .local import *
 
 if os.getenv("GITHUB_WORKFLOW"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "github-actions",
-            "USER": "postgres",
-            "PASSWORD": "postgres",
-            "HOST": "localhost",
-            "PORT": "5432",
-        }
-    }
-
     ELASTICSEARCH_DSL = {
         "default": {"hosts": "elasticsearch"},
     }
